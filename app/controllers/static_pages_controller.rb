eb288b29a13ba23c832db669ea7fd34893d2dea2
Kenneth Lee <kenlee@mdsol.com>
2023-01-09 13:51:23 -0800

    When we hit a route, the corresponding method in the corresponding controller executes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    puts 'someone visited the home page!'
   end
-
+  
   def help
+    puts 'someone visited the home page!'
   end
 end
